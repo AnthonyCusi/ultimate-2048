2e3733b84e471ce2f4139ca98cd016905ae4901e
Amanda Hausmann <anhausma@uci.edu>
2025-03-07 15:42:59 -0800

    minor a2c optimizations

--- src/run_script.sh
+++ src/run_script.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-'''
-To run model multiple times:
-1. change the letter below to desired model
-2. run chmod +x src/run_script.sh
-3. run ./src/run_script.sh
-'''
+# To run model multiple times:
+# 1. change the letter below to desired model
+# 2. run chmod +x src/run_script.sh
+# 3. run ./src/run_script.sh
 
 for i in {1..10}
 do
-    echo "Running simulation $i..."
+    printf "\nRunning simulation $i...\n"
     echo -e "a" | python3 src/run_simulation.py
 done
